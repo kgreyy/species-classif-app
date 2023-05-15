88d58c7a36c7733b3c587547cdd6bd15d3966bb2
kgreyy <raynor@t1v-n-ba766e9d-w-0.us-central1-f.c.taxonomy-classifier.internal>
2023-05-15 06:13:32 +0000

    Add functionality and UI fixes

--- species-classif.py
+++ species-classif.py
@@ -5,14 +5,14 @@ import numpy as np
 import base64
 
 import argparse
-# import jax
+import jax
 import json
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
 
 from PIL import Image
-# from transformers import CLIPProcessor, FlaxCLIPModel
+from transformers import CLIPProcessor, FlaxCLIPModel
 
 
 @st.cache_resource()
@@ -26,7 +26,8 @@ def set_png_as_page_bg(png_file):
     page_bg_img = '''
     <style>
     .stApp {
-    background-image: url("data:image/png;base64,%s");
+    background-color: black;
+    /*background-image: url("data:image/png;base64,%s");*/
     background-size: cover;
     background-repeat: no-repeat;
     background-attachment: scroll; # doesn't work
@@ -38,7 +39,7 @@ def set_png_as_page_bg(png_file):
     return
 
 def predict_one_image(image_file, model, processor, class_names, k, order):
-    eval_image = Image.fromarray(plt.imread(os.path.join(IMAGES_DIR, image_file)))
+    eval_image = image_file
     eval_sentences = ["Belongs to {:s} {:s}".format(order, ct) for ct in class_names]
     inputs = processor(text=eval_sentences,
                        images=eval_image,
@@ -60,35 +61,72 @@ BASELINE_MODEL = "openai/clip-vit-base-patch32"
 MODEL_DIR = "/home/shared/models/clip-rsicd"
 K_VALUES = [1, 3, 5, 10]
 
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
 
 if __name__=='__main__':
+    os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"]="false"
+    os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"]=".XX"
+    os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"]="platform"
+
     set_png_as_page_bg('white-concrete-wall.jpg')
     df_categs = pd.read_csv("data/test_hierarchy_reference.csv")
 
-    st.markdown('<h1 style="color:black;">CLIP Species Classification model</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 style="color:white;">CLIP Taxon Classification model</h1>', unsafe_allow_html=True)
     st.markdown('<h2 style="color:gray;">The image classification model classifies images according to:</h2>', unsafe_allow_html=True)
     st.markdown('<h3 style="color:gray;">'+', '.join(df_categs.columns)+'</h3>', unsafe_allow_html=True)
 
     
 
     class_col = st.selectbox('Select what to classify on', df_categs.columns, len(df_categs.columns)-1)
-    st.markdown('### You have selected ' + class_col)
-    st.markdown('<h3 style="color:black;"> There are '+str(len(df_categs[class_col].unique()))+' classes of taxonomic classification: '+class_col+'.</h3>', unsafe_allow_html=True)
+    st.markdown('<h3 style="color:gray;">You have selected ' + class_col +'.</h3>', unsafe_allow_html=True)
+    st.markdown('<h3 style="color:gray;">There are '+str(len(df_categs[class_col].unique()))+' classes of taxonomic classification: '+class_col+'.</h3>', unsafe_allow_html=True)
 
     # Image upload
     upload= st.file_uploader('Insert image for classification', type=['png','jpg'])
     c1, c2= st.columns(2)
     if upload is not None:
-        im= Image.open(upload)
-        img= np.asarray(im)
-        image= cv2.resize(img,(224, 224))
-        img= np.expand_dims(img, 0)
+        im = Image.open(upload)
+        img = np.asarray(im)
+        img = image_resize(img, height = 720)
+        img = np.expand_dims(img, 0)
         c1.header('Input Image')
         c1.image(im)
         c1.write(img.shape)
         model = FlaxCLIPModel.from_pretrained(BASELINE_MODEL)
         processor = CLIPProcessor.from_pretrained(BASELINE_MODEL)
-        class_names = df_categs[class_col].unique()
+        class_names = df_categs[class_col].dropna().unique()
         preds = predict_one_image(
                 img, model, processor, class_names, max(K_VALUES), class_col)
-        st.markdown('<h3 style="color:black;"> The model predicted: '+'\n'.join(['\t'.join(r) for r in preds])+'</h3>')
+        st.markdown('<h3 style="color:white;"> The model predicted: </h3>',unsafe_allow_html=True)
+        df = pd.DataFrame(preds, columns=['pred', 'probability'])
+        st.table(df)
